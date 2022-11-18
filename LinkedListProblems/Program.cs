72e035955ec2acbe29fde726809a68058ac749c1
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-11-18 16:21:37 +0530

    (Shubham)Add: create linked list

--- LinkedListProblems/Program.cs
+++ LinkedListProblems/Program.cs
@@ -5,7 +5,30 @@ namespace LinkedListProblems
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the LinkedList Problems");
+            LinkedList list = new LinkedList();
+            Console.WriteLine("Welcome to the LinkedList Operations");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\n_________________________________________________");
+                Console.WriteLine("Select the operation to perform\n1.Create LinkedList\n2.Display\n3.Exit");
+                int choice=Convert.ToInt32(Console.ReadLine());
+                switch(choice)
+                {
+                    case 1:
+                        list.Add(56);
+                        list.Add(30);
+                        list.Add(70);
+                        break;
+                    case 2:
+                        list.Display();
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
+                }
+
+            }
         }
     }
 }
