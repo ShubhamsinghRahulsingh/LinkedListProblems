a32fe284a26c5bebc80bb375319c0a96621ec3d5
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-11-18 23:51:48 +0530

    (Shubham)Add: Search node value in linkedlist

--- LinkedListProblems/LinkedList.cs
+++ LinkedListProblems/LinkedList.cs
@@ -95,6 +95,23 @@ namespace LinkedListProblems
             lastNode.next = null;
             Console.WriteLine("Last element is deleted successfully");
         }
+        public int Search(int value)
+        {
+            Node node = this.head;
+            if (node == null)
+                return -1;
+            int count = 0;
+            while (node != null)
+            {
+                if (node.data == value)
+                {
+                    return count;
+                }
+                node = node.next;
+                count++;
+            }
+            return count;
+        }
         public void Display()
         {
             Node temp = this.head;
--- LinkedListProblems/Program.cs
+++ LinkedListProblems/Program.cs
@@ -7,7 +7,7 @@ namespace LinkedListProblems
         {
             LinkedList list = new LinkedList();
             Console.WriteLine("Welcome to the LinkedList Operations");
-            Console.WriteLine("Select the operation to perform\n1.Create LinkedList\n2.Add Data In Reverse Order\n3.Insert At Particular Place in LinkedList\n4.Remove First Node\n5.Remove Last Node");
+            Console.WriteLine("Select the operation to perform\n1.Create LinkedList\n2.Add Data In Reverse Order\n3.Insert At Particular Place in LinkedList\n4.Remove First Node\n5.Remove Last Node\n6.Search Node Value in LinkedList");
             Console.Write("Enter your choice: ");
             int choice=Convert.ToInt32(Console.ReadLine());
             switch (choice)
@@ -44,6 +44,13 @@ namespace LinkedListProblems
                     list.Add(70);
                     list.RemoveLastNode();
                     list.Display();
+                    break;
+                case 6:
+                    list.Add(56);
+                    list.Add(30);
+                    list.Add(70);
+                    int find=list.Search(30);
+                    Console.WriteLine("The Position of Node with value 30 in LinkedList is "+find);
                     break;
                 default:
                     Console.WriteLine("Invalid Input");
