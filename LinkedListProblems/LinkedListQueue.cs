db7f273f2a00429a5c3b28e04fa5fb6867701990
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-11-25 13:39:32 +0530

    (Shubham)Add: LinkedList with Stack and queue methods

---
+++ LinkedListProblems/LinkedListQueue.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProblems
+{
+    public class LinkedListQueue
+    {
+        Node head = null;
+        public void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if(head==null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next!=null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} is inserted into Queue", node.data);
+        }
+        public void Dequeue()
+        {
+            if(this.head==null)
+            {
+                Console.WriteLine("Queue is Empty.So Deletion is not Possible");
+                return;
+            }
+            else
+            {
+                this.head = this.head.next;
+            }
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if(temp==null)
+            {
+                Console.WriteLine("Queue is Empty");
+                return;
+            }
+            Console.WriteLine("Queue is as:");
+            while(temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
--- LinkedListProblems/Program.cs
+++ LinkedListProblems/Program.cs
@@ -5,15 +5,30 @@ namespace LinkedListProblems
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the LinkedList Operations");
-            LinkedList list = new LinkedList();
-            list.Add(56);
-            list.Add(30);
-            list.Add(70);
-            list.Display();
-            int position = list.Search(30);
-            list.InsertAtParticuarPosition(position + 1, 40);
-            list.Display();
+            //Console.WriteLine("Welcome to the LinkedList Operations");
+           // LinkedList list = new LinkedList();
+            //list.Add(56);
+            //list.Add(30);
+            //list.Add(70);
+            //list.Display();
+            //int position = list.Search(30);
+            //list.InsertAtParticuarPosition(position + 1, 40);
+            //list.Display();
+            LinkedListStack stack = new LinkedListStack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+           stack.Display();
+            //  stack.Peek();
+            //stack.Pop();
+            //stack.Display();
+            //LinkedListQueue queue=new LinkedListQueue();
+            //queue.Enqueue(56);
+            //queue.Enqueue(30);
+            //queue.Enqueue(70);
+            //queue.Display();
+            //queue.Dequeue();
+            //queue.Display();
         }
     }
 }
